e99cf992b4ec9dc8f4cbac279d0608cea9388132
Lucas Micheloni <lucas.michelonidesouza@gmail.com>
2019-04-22 23:11:00 -0300

    add bing.sh, my bing wallpaper daily fetcher

---
+++ .bing.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+today=$(date +%Y:%m:%d:%H)
+x="$HOME/.bing-wallpapers/$today.jpg"
+url=$(curl "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1" | jq .images[0].url | tr -d '",')
+
+wget -O $x "https://bing.com$url"
+
+feh --bg-scale $x
