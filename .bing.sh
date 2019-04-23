058076dcec3a991d1a4bc47d9e840625a9347b38
Lucas Micheloni <lucas.michelonidesouza@gmail.com>
2019-04-22 23:11:47 -0300

    little mini edit on bing.sh, my wallpaper fetcher

--- .bing.sh
+++ .bing.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-today=$(date +%Y:%m:%d:%H)
+today=$(date +%Y:%m:%d)
 x="$HOME/.bing-wallpapers/$today.jpg"
 url=$(curl "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1" | jq .images[0].url | tr -d '",')
 
